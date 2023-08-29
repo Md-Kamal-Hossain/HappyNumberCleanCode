caaca340a893440eb11f2799a282f5babf541b1d
Lenovo <Lenovo@DESKTOP-KSE7MLT>
2023-08-29 19:58:26 +0200

    Add project files.

---
+++ HappyNumberCleanCode/HappyNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyNumberCleanCode
+{
+    public static class HappyNumber
+    {
+        public static int IsHappyNumber(int happyNumber)
+        {
+
+            int sum = 0;int rem = 0;    
+            while (happyNumber>0) 
+            {
+                rem = happyNumber % 10;
+                sum = sum+ rem*rem;
+                happyNumber =happyNumber / 10;  
+            }
+            return sum;
+            
+
+
+
+
+        }
+       
+
+    }
+}
---
+++ HappyNumberCleanCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HappyNumberCleanCode 
+{
+    public class Program
+    {
+       public  static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a Number : ");
+            int Number = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Happy Number"  + " : ");
+            
+            int result = Number;
+          
+            while (result != 1 && result != 4)
+            {
+                result = HappyNumber.IsHappyNumber(result);
+            }                 
+            if (result == 1)
+            {
+                Console.Write(Number + " ");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
